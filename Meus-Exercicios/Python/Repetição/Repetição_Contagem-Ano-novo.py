0c700bcfee4c82706f2a29a32e529281d6231fbe
Gabriel Jesus <gabrieljesus.w.paula@gmail.com>
2025-09-01 18:06:18 -0300

    .

---
+++ Meus-Exercicios/Python/Repetição/Repetição_Contagem-Ano-novo.py
@@ -0,0 +1,10 @@
+import time
+print("-="*30)
+
+print("Contagem de 10 até 0 para o ano novo: ")  
+
+for c in range(10, 0,-1):
+    print(c)
+    time.sleep(1.0)
+print("FELIZZZZZ ANO NOVOOOOOOOO!!!!!!!")
+print("-="*30)
